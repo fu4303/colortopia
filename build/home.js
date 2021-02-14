56519e4121a947511149214810a972c0c65bf0b3
Bereket Semagn <semagnbereket@gmail.com>
2021-02-13 20:06:41 -0500

    added all color swatches

--- build/home.js
+++ build/home.js
@@ -10,7 +10,7 @@ const colors = [
          { name: "Red 7", color: "#f03e3e" },
          { name: "Red 8", color: "#e03131" },
          { name: "Red 9", color: "#c92a2a" },
-        
+
          // Pink
          { name: "Pink 0", color: "#fff0f6" },
          { name: "Pink 1", color: "#ffdeeb" },
@@ -23,7 +23,55 @@ const colors = [
          { name: "Pink 8", color: "#c2255c" },
          { name: "Pink 9", color: "#a61e4d" },
 
-         // Pink
+         // Yellow
+         { name: "Yellow 0", color: "#fffde7" },
+         { name: "Yellow 1", color: "#fff9c4" },
+         { name: "Yellow 2", color: "#fff59d" },
+         { name: "Yellow 3", color: "#fff176" },
+         { name: "Yellow 4", color: "#ffee58" },
+         { name: "Yellow 5", color: "#ffeb3b" },
+         { name: "Yellow 6", color: "#fdd835" },
+         { name: "Yellow 7", color: "#fbc02d" },
+         { name: "Yellow 8", color: "#f9a825" },
+         { name: "Yellow 9", color: "#f57f17" },
+
+         // Amber
+         { name: "Amber 0", color: "#fff8e1" },
+         { name: "Amber 1", color: "#ffecb3" },
+         { name: "Amber 2", color: "#ffe082" },
+         { name: "Amber 3", color: "#ffd54f" },
+         { name: "Amber 4", color: "#ffca28" },
+         { name: "Amber 5", color: "#ffc107" },
+         { name: "Amber 6", color: "#ffb300" },
+         { name: "Amber 7", color: "#ffa000" },
+         { name: "Amber 8", color: "#ff8f00" },
+         { name: "Amber 9", color: "#ff6f00" },
+
+         // Orange
+         { name: "Orange 0", color: "#fff4e6" },
+         { name: "Orange 1", color: "#ffe8cc" },
+         { name: "Orange 2", color: "#ffd8a8" },
+         { name: "Orange 3", color: "#ffc078" },
+         { name: "Orange 4", color: "#ffa94d" },
+         { name: "Orange 5", color: "#ff922b" },
+         { name: "Orange 6", color: "#fd7e14" },
+         { name: "Orange 7", color: "#f76707" },
+         { name: "Orange 8", color: "#e8590c" },
+         { name: "Orange 9", color: "#d9480f" },
+
+         // Deep Orange
+         { name: "Deep Orange 0", color: "#fbe9e7" },
+         { name: "Deep Orange 1", color: "#ffccbc" },
+         { name: "Deep Orange 2", color: "#ffab91" },
+         { name: "Deep Orange 3", color: "#ff8a65" },
+         { name: "Deep Orange 4", color: "#ff7043" },
+         { name: "Deep Orange 5", color: "#ff5722" },
+         { name: "Deep Orange 6", color: "#f4511e" },
+         { name: "Deep Orange 7", color: "#e64a19" },
+         { name: "Deep Orange 8", color: "#d84315" },
+         { name: "Deep Orange 9", color: "#bf360c" },
+
+         // Grape
          { name: "Grape 0", color: "#f8f0fc" },
          { name: "Grape 1", color: "#f3d9fa" },
          { name: "Grape 2", color: "#eebefa" },
@@ -35,17 +83,149 @@ const colors = [
          { name: "Grape 8", color: "#9c36b5" },
          { name: "Grape 9", color: "#862e9c" },
 
-        // Gray
-        { name: "Gray 0", color: "#f8f9fa" },
-        { name: "Gray 1", color: "#f1f3f5" },
-        { name: "Gray 2", color: "#e9ecef" },
-        { name: "Gray 3", color: "#dee2e6" },
-        { name: "Gray 4", color: "#ced4da" },
-        { name: "Gray 5", color: "#adb5bd" },
-        { name: "Gray 6", color: "#868e96" },
-        { name: "Gray 7", color: "#495057" },
-        { name: "Gray 8", color: "#343a40" },
-        { name: "Gray 9", color: "#212529" },
+         // Violet
+         { name: "Violet 0", color: "#f3f0ff" },
+         { name: "Violet 1", color: "#e5dbff" },
+         { name: "Violet 2", color: "#d0bfff" },
+         { name: "Violet 3", color: "#b197fc" },
+         { name: "Violet 4", color: "#9775fa" },
+         { name: "Violet 5", color: "#845ef7" },
+         { name: "Violet 6", color: "#7950f2" },
+         { name: "Violet 7", color: "#7048e8" },
+         { name: "Violet 8", color: "#6741d9" },
+         { name: "Violet 9", color: "#5f3dc4" },
+
+         // Purple
+         { name: "Purple 0", color: "#f3e5f5" },
+         { name: "Purple 1", color: "#e1bee7" },
+         { name: "Purple 2", color: "#ce93d8" },
+         { name: "Purple 3", color: "#ba68c8" },
+         { name: "Purple 4", color: "#ab47bc" },
+         { name: "Purple 5", color: "#9c27b0" },
+         { name: "Purple 6", color: "#8e24aa" },
+         { name: "Purple 7", color: "#7b1fa2" },
+         { name: "Purple 8", color: "#6a1b9a" },
+         { name: "Purple 9", color: "#4a148c" },
+
+         // Indigo
+         { name: "Indigo 0", color: "#edf2ff" },
+         { name: "Indigo 1", color: "#dbe4ff" },
+         { name: "Indigo 2", color: "#bac8ff" },
+         { name: "Indigo 3", color: "#91a7ff" },
+         { name: "Indigo 4", color: "#748ffc" },
+         { name: "Indigo 5", color: "#5c7cfa" },
+         { name: "Indigo 6", color: "#4c6ef5" },
+         { name: "Indigo 7", color: "#4263eb" },
+         { name: "Indigo 8", color: "#3b5bdb" },
+         { name: "Indigo 9", color: "#364fc7" },
+
+         // Blue
+         { name: "Blue 0", color: "#e7f5ff" },
+         { name: "Blue 1", color: "#d0ebff" },
+         { name: "Blue 2", color: "#a5d8ff" },
+         { name: "Blue 3", color: "#74c0fc" },
+         { name: "Blue 4", color: "#4dabf7" },
+         { name: "Blue 5", color: "#339af0" },
+         { name: "Blue 6", color: "#228be6" },
+         { name: "Blue 7", color: "#1c7ed6" },
+         { name: "Blue 8", color: "#1971c2" },
+         { name: "Blue 9", color: "#1864ab" },
+
+         // Cyan
+         { name: "Cyan 0", color: "#e3fafc" },
+         { name: "Cyan 1", color: "#c5f6fa" },
+         { name: "Cyan 2", color: "#99e9f2" },
+         { name: "Cyan 3", color: "#66d9e8" },
+         { name: "Cyan 4", color: "#3bc9db" },
+         { name: "Cyan 5", color: "#22b8cf" },
+         { name: "Cyan 6", color: "#15aabf" },
+         { name: "Cyan 7", color: "#1098ad" },
+         { name: "Cyan 8", color: "#0c8599" },
+         { name: "Cyan 9", color: "#0b7285" },
+
+         // Teal
+         { name: "Teal 0", color: "#e6fcf5" },
+         { name: "Teal 1", color: "#c3fae8" },
+         { name: "Teal 2", color: "#96f2d7" },
+         { name: "Teal 3", color: "#63e6be" },
+         { name: "Teal 4", color: "#38d9a9" },
+         { name: "Teal 5", color: "#20c997" },
+         { name: "Teal 6", color: "#12b886" },
+         { name: "Teal 7", color: "#0ca678" },
+         { name: "Teal 8", color: "#099268" },
+         { name: "Teal 9", color: "#087f5b" },
+
+         // Green
+         { name: "Green 0", color: "#e8f5e9" },
+         { name: "Green 1", color: "#c8e6c9" },
+         { name: "Green 2", color: "#a5d6a7" },
+         { name: "Green 3", color: "#81c784" },
+         { name: "Green 4", color: "#66bb6a" },
+         { name: "Green 5", color: "#4caf50" },
+         { name: "Green 6", color: "#43a047" },
+         { name: "Green 7", color: "#388e3c" },
+         { name: "Green 8", color: "#2e7d32" },
+         { name: "Green 9", color: "#1b5e20" },
+
+         // Light Green
+         { name: "Light Green 0", color: "#ebfbee" },
+         { name: "Light Green 1", color: "#d3f9d8" },
+         { name: "Light Green 2", color: "#b2f2bb" },
+         { name: "Light Green 3", color: "#8ce99a" },
+         { name: "Light Green 4", color: "#69db7c" },
+         { name: "Light Green 5", color: "#51cf66" },
+         { name: "Light Green 6", color: "#40c057" },
+         { name: "Light Green 7", color: "#37b24d" },
+         { name: "Light Green 8", color: "#2f9e44" },
+         { name: "Light Green 9", color: "#2b8a3e" },
+
+         // Lime
+         { name: "Lime 0", color: "#f4fce3" },
+         { name: "Lime 1", color: "#e9fac8" },
+         { name: "Lime 2", color: "#d8f5a2" },
+         { name: "Lime 3", color: "#c0eb75" },
+         { name: "Lime 4", color: "#a9e34b" },
+         { name: "Lime 5", color: "#94d82d" },
+         { name: "Lime 6", color: "#82c91e" },
+         { name: "Lime 7", color: "#74b816" },
+         { name: "Lime 8", color: "#66a80f" },
+         { name: "Lime 9", color: "#5c940d" },
+
+         // Brown
+         { name: "Brown 0", color: "#efebe9" },
+         { name: "Brown 1", color: "#d7ccc8" },
+         { name: "Brown 2", color: "#bcaaa4" },
+         { name: "Brown 3", color: "#a1887f" },
+         { name: "Brown 4", color: "#8d6e63" },
+         { name: "Brown 5", color: "#795548" },
+         { name: "Brown 6", color: "#6d4c41" },
+         { name: "Brown 7", color: "#5d4037" },
+         { name: "Brown 8", color: "#4e342e" },
+         { name: "Brown 9", color: "#3e2723" },
+
+         // Gray
+         { name: "Gray 0", color: "#f8f9fa" },
+         { name: "Gray 1", color: "#f1f3f5" },
+         { name: "Gray 2", color: "#e9ecef" },
+         { name: "Gray 3", color: "#dee2e6" },
+         { name: "Gray 4", color: "#ced4da" },
+         { name: "Gray 5", color: "#adb5bd" },
+         { name: "Gray 6", color: "#868e96" },
+         { name: "Gray 7", color: "#495057" },
+         { name: "Gray 8", color: "#343a40" },
+         { name: "Gray 9", color: "#212529" },
+
+         // Blue Gray
+         { name: "Blue Gray 0", color: "#eceff1" },
+         { name: "Blue Gray 1", color: "#cfd8dc" },
+         { name: "Blue Gray 2", color: "#b0bec5" },
+         { name: "Blue Gray 3", color: "#90a4ae" },
+         { name: "Blue Gray 4", color: "#78909c" },
+         { name: "Blue Gray 5", color: "#607d8b" },
+         { name: "Blue Gray 6", color: "#546e7a" },
+         { name: "Blue Gray 7", color: "#455a64" },
+         { name: "Blue Gray 8", color: "#37474f" },
+         { name: "Blue Gray 9", color: "#263238" },
 
         
       ];
